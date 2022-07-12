d289f74a9957c57e5f47530d2863823681c8d529
ipora <possantti@gmail.com>
2022-07-12 11:48:51 -0300

    formatting

--- cue1d.py
+++ cue1d.py
@@ -1,4 +1,4 @@
-'''
+"""
 
 CUE 1d routines source code
 
@@ -41,20 +41,13 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-'''
+"""
 import numpy as np
 import pandas as pd
 import backend
 
 
-def play(
-        df_agents,
-        df_places,
-        n_steps,
-        b_tui=False,
-        b_return=False,
-        b_trace=True
-    ):
+def play(df_agents, df_places, n_steps, b_tui=False, b_return=False, b_trace=True):
     """
     Run the CUE 1d simulation
     :param df_agents: pandas dataframe of agents
@@ -66,19 +59,18 @@ def play(
     :return: output dictionary
     """
     # start up
-    s_dtype = 'float32'
+    s_dtype = "float32"
     n_agents = len(df_agents)
     n_places = len(df_places)
 
     # get unique beta values
-    lst_unique_beta = list(df_agents['Beta'].unique())
+    lst_unique_beta = list(df_agents["Beta"].unique())
     dct_window = dict()
     for n_beta in lst_unique_beta:
         # get scanning window parameters
-        vct_window_rows, vct_window_cols = backend.get_window_ids(n_rows=n_places,
-                                                                  n_cols=n_places,
-                                                                  n_rsize=n_beta,
-                                                                  b_flat=False)
+        vct_window_rows, vct_window_cols = backend.get_window_ids(
+            n_rows=n_places, n_cols=n_places, n_rsize=n_beta, b_flat=False
+        )
         if b_return:
             lst_aux = list(vct_window_cols[0])
         else:
@@ -90,123 +82,135 @@ def play(
                 else:
                     lst_aux.append(e)
         # get window base ids
-        vct_rows_base_ids = np.array(lst_aux, dtype='uint16')
+        vct_rows_base_ids = np.array(lst_aux, dtype="uint16")
 
         # deploy recyclable window dataframe
         n_window_size = len(vct_rows_base_ids)
         df_window = pd.DataFrame(
             {
-            'x': np.zeros(n_window_size, dtype='uint16'),
-            'Trait': np.zeros(n_window_size, dtype=s_dtype),
-            'D': np.zeros(n_window_size, dtype=s_dtype),
-            'Discrepancy': np.zeros(n_window_size, dtype=s_dtype),
-            'Interac_score': np.zeros(n_window_size, dtype=s_dtype),
-            'Interac_prob': np.zeros(n_window_size, dtype=s_dtype)
+                "x": np.zeros(n_window_size, dtype="uint16"),
+                "Trait": np.zeros(n_window_size, dtype=s_dtype),
+                "D": np.zeros(n_window_size, dtype=s_dtype),
+                "Discrepancy": np.zeros(n_window_size, dtype=s_dtype),
+                "Interac_score": np.zeros(n_window_size, dtype=s_dtype),
+                "Interac_prob": np.zeros(n_window_size, dtype=s_dtype),
             }
         )
         # append to dict
-        dct_window[str(n_beta)] = {
-            'ids': vct_rows_base_ids,
-            'df': df_window
-        }
+        dct_window[str(n_beta)] = {"ids": vct_rows_base_ids, "df": df_window}
 
     # define random seeds prior to simulation loop
     np.random.seed(backend.get_seed())
-    grd_seeds = np.random.randint(low=100, high=999, size=(n_steps, n_agents), dtype='uint16')
+    grd_seeds = np.random.randint(
+        low=100, high=999, size=(n_steps, n_agents), dtype="uint16"
+    )
 
     # tracing variables
     if b_trace:
         grd_traced_agents_x = np.zeros(shape=(n_steps, n_agents), dtype=s_dtype)
         grd_traced_agents_traits = np.zeros(shape=(n_steps, n_agents), dtype=s_dtype)
         grd_traced_places_traits = np.zeros(shape=(n_steps, n_places), dtype=s_dtype)
-        grd_traced_places_traits[0] = df_places['Trait'].values
-        grd_traced_agents_traits[0] = df_agents['Trait'].values
-        grd_traced_agents_x[0] = df_agents['x'].values
+        grd_traced_places_traits[0] = df_places["Trait"].values
+        grd_traced_agents_traits[0] = df_agents["Trait"].values
+        grd_traced_agents_x[0] = df_agents["x"].values
 
     if b_return:
         df_agents_copy = df_agents.copy()
-        vct_agents_origin_x = df_agents_copy['x'].values
+        vct_agents_origin_x = df_agents_copy["x"].values
 
     # main simulation loop
     for t in range(1, n_steps):
         if b_tui:
-            backend.status('CUE1d :: simulation step {} [{:.2f}%]'.format(t, 100 * t / n_steps))
+            backend.status(
+                "CUE1d :: simulation step {} [{:.2f}%]".format(t, 100 * t / n_steps)
+            )
         # agents movements
         for a in range(n_agents):
             # get agent variables
-            n_crt_agent_trait = df_agents['Trait'].values[a]
-            n_crt_agent_x = df_agents['x'].values[a]
+            n_crt_agent_trait = df_agents["Trait"].values[a]
+            n_crt_agent_x = df_agents["x"].values[a]
 
             # get agent parameters
-            n_crt_alpha = df_agents['Alpha'].values[a]
-            n_crt_agent_beta = df_agents['Beta'].values[a]
-            n_crt_agent_c = df_agents['C'].values[a]
+            n_crt_alpha = df_agents["Alpha"].values[a]
+            n_crt_agent_beta = df_agents["Beta"].values[a]
+            n_crt_agent_c = df_agents["C"].values[a]
 
             # get current window objects
-            vct_rows_base_ids = dct_window[str(n_crt_agent_beta)]['ids']
-            df_window = dct_window[str(n_crt_agent_beta)]['df']
+            vct_rows_base_ids = dct_window[str(n_crt_agent_beta)]["ids"]
+            df_window = dct_window[str(n_crt_agent_beta)]["df"]
 
             # update window dataframe
-            df_window['x'] = (n_crt_agent_x + vct_rows_base_ids) % n_places
-            df_window['Trait'] = df_places['Trait'].values[df_window['x'].values]
-            df_window['D'] = df_places['D'].values[df_window['x'].values]
-            df_window['Discrepancy'] = np.abs(n_crt_agent_trait - df_window['Trait'].values)
+            df_window["x"] = (n_crt_agent_x + vct_rows_base_ids) % n_places
+            df_window["Trait"] = df_places["Trait"].values[df_window["x"].values]
+            df_window["D"] = df_places["D"].values[df_window["x"].values]
+            df_window["Discrepancy"] = np.abs(
+                n_crt_agent_trait - df_window["Trait"].values
+            )
 
             # compute selection Interac_score
-            df_window['Interac_score'] = df_window['Discrepancy'].max() - df_window['Discrepancy'] + 1  # normalize
+            df_window["Interac_score"] = (
+                df_window["Discrepancy"].max() - df_window["Discrepancy"] + 1
+            )  # normalize
             # set score = zero where place is beyond alpha threshold
-            df_window['Interac_score'] = df_window['Interac_score'].values * (
-                    df_window['Discrepancy'].values <= n_crt_alpha)
+            df_window["Interac_score"] = df_window["Interac_score"].values * (
+                df_window["Discrepancy"].values <= n_crt_alpha
+            )
 
             # compute probabilistic weights
-            if df_window['Interac_score'].sum() == 0:
-                df_window['Interac_prob'] = 1 / len(df_window)  # uniform distribution when all scores == 0
+            if df_window["Interac_score"].sum() == 0:
+                df_window["Interac_prob"] = 1 / len(
+                    df_window
+                )  # uniform distribution when all scores == 0
             else:
-                df_window['Interac_prob'] = df_window['Interac_score'] / df_window['Interac_score'].sum()
+                df_window["Interac_prob"] = (
+                    df_window["Interac_score"] / df_window["Interac_score"].sum()
+                )
 
             # move agent
             np.random.seed(grd_seeds[t, a])  # restart random state
             # weighted random sampling
-            n_next_index = np.random.choice(a=df_window.index, size=1, p=df_window['Interac_prob'].values)[0]
+            n_next_index = np.random.choice(
+                a=df_window.index, size=1, p=df_window["Interac_prob"].values
+            )[0]
 
             # update agent position
-            df_agents['x'].values[a] = df_window['x'].values[n_next_index]
+            df_agents["x"].values[a] = df_window["x"].values[n_next_index]
 
             # apply interaction criteria
-            if df_window['Interac_score'].values[n_next_index] > 0:
+            if df_window["Interac_score"].values[n_next_index] > 0:
                 # get space parameters from window dataframe
-                n_crt_place_trait = df_window['Trait'].values[n_next_index]
-                n_crt_place_x = df_window['x'].values[n_next_index]
-                n_crt_place_d = df_window['D'].values[n_next_index]
+                n_crt_place_trait = df_window["Trait"].values[n_next_index]
+                n_crt_place_x = df_window["x"].values[n_next_index]
+                n_crt_place_d = df_window["D"].values[n_next_index]
 
                 # compute means
-                r_mean_agent = (n_crt_agent_trait + (n_crt_agent_c * n_crt_place_trait)) / (1 + n_crt_agent_c)
+                r_mean_agent = (
+                    n_crt_agent_trait + (n_crt_agent_c * n_crt_place_trait)
+                ) / (1 + n_crt_agent_c)
 
-                r_mean_place = (n_crt_place_trait + (n_crt_place_d * n_crt_agent_trait)) / (1 + n_crt_place_d)
+                r_mean_place = (
+                    n_crt_place_trait + (n_crt_place_d * n_crt_agent_trait)
+                ) / (1 + n_crt_place_d)
                 # replace in simulation dataframes
-                df_agents['Trait'].values[a] = r_mean_agent
-                df_places['Trait'].values[n_crt_place_x] = r_mean_place
+                df_agents["Trait"].values[a] = r_mean_agent
+                df_places["Trait"].values[n_crt_place_x] = r_mean_place
         # trace
         if b_trace:
-            grd_traced_places_traits[t] = df_places['Trait'].values
-            grd_traced_agents_traits[t] = df_agents['Trait'].values
-            grd_traced_agents_x[t] = df_agents['x'].values
+            grd_traced_places_traits[t] = df_places["Trait"].values
+            grd_traced_agents_traits[t] = df_agents["Trait"].values
+            grd_traced_agents_x[t] = df_agents["x"].values
         # reset x values
         if b_return:
             # reset x
-            df_agents['x'] = vct_agents_origin_x
+            df_agents["x"] = vct_agents_origin_x
 
     # prepare output dict
-    dct_output = {
-        'Agents': df_agents,
-        'Places': df_places
-    }
+    dct_output = {"Agents": df_agents, "Places": df_places}
     # append extra content
     if b_trace:
-        dct_output['Simulation'] = {
-            'Places_traits': grd_traced_places_traits,
-            'Agents_traits': grd_traced_agents_traits,
-            'Agents_x': grd_traced_agents_x
+        dct_output["Simulation"] = {
+            "Places_traits": grd_traced_places_traits,
+            "Agents_traits": grd_traced_agents_traits,
+            "Agents_x": grd_traced_agents_x,
         }
     return dct_output
-
